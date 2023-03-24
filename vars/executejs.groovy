1dd55c68a7fe77cc8a7e2919b80cdca5cbccf515
Clem34280 <86777788+Clem34280@users.noreply.github.com>
2023-03-24 16:44:11 +0100

    Update executejs.groovy

--- vars/executejs.groovy
+++ vars/executejs.groovy
@@ -3,12 +3,5 @@ import java.nio.file.Paths
 def call(String scriptFilename, String workspace){
     String scriptPath = [workspace, 'test', scriptFilename].join('/')
 
-    if(isUnix())
-	{
-		sh "node ${scriptPath}  ${workspace}"
-	}
-	else
-	{
-		bat "node ${scriptPath}  ${workspace}"
-	}
+    bat "mocha ${scriptPath}  ${workspace}"
 }
